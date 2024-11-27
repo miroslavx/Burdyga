01fa9e72f722002b8954b5c447e4645319b7d46e
miroslavx <56218543+miroslavx@users.noreply.github.com>
2024-11-27 14:50:48 +0200

    Update Praktiline_töö.py
    Signed-off-by: miroslavx <56218543+miroslavx@users.noreply.github.com>

--- Burdyga/Praktiline_töö.py
+++ Burdyga/Praktiline_töö.py
@@ -1,4 +1,26 @@
-﻿from datetime import *
+from datetime import *
 
 tana=date.today()g
-print(f"Täna on {tana}")
+tana=date.today().strftime("%B %d, %Y")
+
+print(f"Täna on {tanaf}")
+d=tana.day
+m=tana.month
+y=tana.year
+print(d)
+print(m)
+print(y)
+
+#Ülesanne 1
+
+print("Tere tulemast!")
+nimi=input("Mis on sinu nimi? ").capitalize() #lower()-aaa, upper()-AAA, capitalize
+()-Aaa
+print("Tere tulemast! Tervitan sind ", nimi)
+print("Tere tulemast! Tervitan sind "+ nimi)
+try:
+   vanus=int(input("Kui vana sa oled? "))
+   print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ", vanus, "aastat vana")
+   print(f"\tTere tulemast! \nTervitan sind {nimi) Sa oled {vanus} aastat vana")
+except:
+    print("On vaja numbreid sisestdal")
