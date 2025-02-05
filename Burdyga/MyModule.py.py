4872ef7dc76b5fbc85a1b820867f0dec9de841ac
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-02-05 14:42:31 +0200

    Update MyModule.py.py
    Signed-off-by: miroslavx <56218543+miroslavx@users.noreply.github.com>

--- Burdyga/MyModule.py.py
+++ Burdyga/MyModule.py.py
@@ -1,96 +1,98 @@
-def registreerimine(loginid, paroolid):
-    kasutajanimi = input("Sisestage uus kasutajanimi: ")
-    if kasutajanimi in loginid:
-        print("Kasutajanimi on juba olemas!")
-        return
+import random
 
-    print("Valige parooli loomise viis:")
-    print("1. Automaatne parooli genereerimine")
-    print("2. Sisestage parool ise")
-    valik = input("Teie valik (1 või 2): ")
-
-    if valik == "1":
-        parool = loo_parool()
-        print("Teie automaatselt genereeritud parool on:", parool)
-    elif valik == "2":
-        parool = input("Sisestage parool: ")
-    else:
-        print("Vale valik!")
-        return
-
-    loginid.append(kasutajanimi)
-    paroolid.append(parool)
-    print("Registreerimine õnnestus!")
-
-
-def loo_parool():
+def genereeri_parool():
     str0 = ".,:;!_*-+()/#¤%&"
-    str1 = "0123456789"
-    str2 = "qwertyuiopasdfghjklzxcvbnm"
-    str3 = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    str1 = '0123456789'
+    str2 = 'qwertyuiopasdfghjklzxcvbnm'
+    str3 = str2.upper()
     str4 = str0 + str1 + str2 + str3
     ls = list(str4)
-    parool = ""
-    for i in range(12):
-        indeks = (i * 7 + 3) % len(ls)
-        parool += ls[indeks]
+    random.shuffle(ls)
+    parool = ''.join([random.choice(ls) for _ in range(12)])
     return parool
 
+def parooli_kontroll(parool):
+    erimargid = ".,:;!_*-+()/#¤%&"
+    on_num = any(c.isdigit() for c in parool)
+    on_väike = any(c.islower() for c in parool)
+    on_suur = any(c.isupper() for c in parool)
+    on_erimärk = any(c in erimargid for c in parool)
+    return on_num and on_väike and on_suur and on_erimärk
 
-def autoriseerimine(loginid, paroolid):
-    kasutajanimi = input("Sisestage kasutajanimi: ")
-    if kasutajanimi not in loginid:
-        print("Kasutajanime ei leitud!")
+def registreeri(logid, paroolid):
+    kasutaja = input("Sisesta kasutajanimi: ")
+    if kasutaja in logid:
+        print("See kasutajanimi on juba olemas!")
         return
-
-    parool = input("Sisestage parool: ")
-    indeks = loginid.index(kasutajanimi)
-
-    if paroolid[indeks] == parool:
-        print("Autoriseerimine õnnestus!")
+    valik = input("Kas soovid parooli genereerida (G) või ise luua (M)? ").upper()
+    if valik == 'G':
+        parool = genereeri_parool()
+        print(f"Genereeritud parool: {parool}")
+    elif valik == 'M':
+        while True:
+            parool = input("Sisesta oma parool: ")
+            if parooli_kontroll(parool):
+                break
+            else:
+                print("Parool peab sisaldama vähemalt ühte numbrit, väike- ja suurtähte ning erimärki!")
     else:
-        print("Vale parool!")
-
-
-def muuda_andmeid(loginid, paroolid):
-    kasutajanimi = input("Sisestage kasutajanimi: ")
-    if kasutajanimi not in loginid:
-        print("Kasutajanime ei leitud!")
+        print("Vigane valik!")
         return
+    logid.append(kasutaja)
+    paroolid.append(parool)
+    print("Kasutaja registreeritud!")
 
-    parool = input("Sisestage vana parool: ")
-    indeks = loginid.index(kasutajanimi)
-
-    if paroolid[indeks] == parool:
-        print("Mida soovite muuta?")
-        print("1. Kasutajanime")
-        print("2. Parooli")
-        valik = input("Teie valik (1 või 2): ")
-
-        if valik == "1":
-            uus_nimi = input("Sisestage uus kasutajanimi: ")
-            if uus_nimi in loginid:
-                print("Selline nimi on juba kasutusel!")
-                return
-            loginid[indeks] = uus_nimi
-            print("Kasutajanimi muudetud!")
-        elif valik == "2":
-            uus_parool = input("Sisestage uus parool: ")
-            paroolid[indeks] = uus_parool
-            print("Parool muudetud!")
+def autoriseeri(logid, paroolid):
+    kasutaja = input("Sisesta kasutajanimi: ")
+    parool = input("Sisesta parool: ")
+    if kasutaja in logid:
+        indeks = logid.index(kasutaja)
+        if paroolid[indeks] == parool:
+            print("Autoriseerimine õnnestus!")
+            return indeks
         else:
-            print("Vale valik!")
+            print("Vale parool!")
     else:
-        print("Vale parool!")
+        print("Kasutajat ei leitud!")
+    return -1
 
-
-def parooli_taastamine(loginid, paroolid):
-    kasutajanimi = input("Sisestage kasutajanimi: ")
-    if kasutajanimi not in loginid:
-        print("Kasutajanime ei leitud!")
+def muuda_andmeid(logid, paroolid):
+    indeks = autoriseeri(logid, paroolid)
+    if indeks == -1:
         return
+    valik = input("Muuda nime (N) või parooli (P)? ").upper()
+    if valik == 'N':
+        uus_nimi = input("Sisesta uus kasutajanimi: ")
+        if uus_nimi in logid:
+            print("See nimi on juba kasutusel!")
+        else:
+            logid[indeks] = uus_nimi
+            print("Nimi muudetud!")
+    elif valik == 'P':
+        valik = input("Genereeri parool (G) või sisesta ise (M)? ").upper()
+        if valik == 'G':
+            uus_parool = genereeri_parool()
+            print(f"Uus parool: {uus_parool}")
+            paroolid[indeks] = uus_parool
+        elif valik == 'M':
+            while True:
+                uus_parool = input("Sisesta uus parool: ")
+                if parooli_kontroll(uus_parool):
+                    break
+                else:
+                    print("Parool peab sisaldama vähemalt ühte numbrit, väike- ja suurtähte ning erimärki!")
+            paroolid[indeks] = uus_parool
+        else:
+            print("Vigane valik!")
+    else:
+        print("Vigane valik!")
 
-    uus_parool = loo_parool()
-    indeks = loginid.index(kasutajanimi)
-    paroolid[indeks] = uus_parool
-    print("Teie uus parool on:", uus_parool)
+def taasta_parool(logid, paroolid):
+    kasutaja = input("Sisesta kasutajanimi: ")
+    if kasutaja in logid:
+        indeks = logid.index(kasutaja)
+        uus_parool = genereeri_parool()
+        paroolid[indeks] = uus_parool
+        print(f"Uus parool: {uus_parool}")
+    else:
+        print("Kasutajat ei leitud!")
